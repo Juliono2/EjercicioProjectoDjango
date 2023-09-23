04daaef6a6725de365fe5c53fd4dc9e3961030c1
Juliono2 <juliono1999@gmail.com>
2023-09-23 18:18:24 -0500

    Habilitamos el registro

--- primer_aplicacion/students/admin.py
+++ primer_aplicacion/students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Students
 
 # Register your models here.
+@admin.register(Students)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lastname')
