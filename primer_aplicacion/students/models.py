e15789fd30a1abebaddc7fb3782eb5691f397580
Juliono2 <juliono1999@gmail.com>
2023-09-23 18:13:06 -0500

    Ya se migro todas las aplicaciones

--- primer_aplicacion/students/models.py
+++ primer_aplicacion/students/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name = models.CharField(max_length=50, null= False, blank= False)
+    lastname = models.CharField(max_length=50, null=False, blank=False)
--- primer_aplicacion/subjects/models.py
+++ primer_aplicacion/subjects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+# Pa validar que no sea negativo el wacho que entra equisde
+def validar_numero_positivo(value):
+    if value <= 0:
+        raise ValidationError('El número debe ser positivo.')
 
 # Create your models here.
+class Subjects(models.Model):
+    name = models.CharField(max_length=50, null= False, blank= False)
+    credits = models.IntegerField(validators=[validar_numero_positivo], null=False, blank=False)
